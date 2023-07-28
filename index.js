0dd5181a263d2fd34234ccb2920e6f191ba4a6b0
aboura <bouchrabichou638@gmail.com>
2023-07-28 12:53:52 +0200

    completed assignment

---
+++ index.js
@@ -0,0 +1,28 @@
+let a = Math.floor(Math.random()*1000);
+let b= Math.floor(Math.random()*1000);
+function add(a,b) {
+    return a+b;
+}
+function subtract(a,b) {
+    return a-b;
+}
+function multiply(a,b) {
+    return a*b;
+}
+function divide(a,b) {
+    return a/b;
+}
+function increment(a) {
+    return a+1;
+}
+function decrement(a) {
+    return a-1;
+}
+a.toString();
+function makeInt(a) {
+    return parseInt(a,10);
+}
+let n =2.222;
+function preserveDecimal(n) {
+    return parseFloat(n);
+}
